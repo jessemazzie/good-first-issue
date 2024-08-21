80ee47e056dc2cb4b86d6db54873058930fa04bb
jessemazzie <jessemazzie1996@gmail.com>
2024-08-21 12:53:12 -0400

    lint fixes

--- good-first-issue/gunicorn_config.py
+++ good-first-issue/gunicorn_config.py
@@ -2,4 +2,4 @@ workers = 2
 threads = 4
 bind = "0.0.0.0:8000"
 forward_allow_ips = "*"
-secure_scheme_headers = { "X-Forwarded-Proto": "https" }
+secure_scheme_headers = {"X-Forwarded-Proto": "https"}
